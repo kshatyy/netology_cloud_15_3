5609a59e3d83b658006ffb6951d39a7c65ad75e1
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-09-13 10:19:21 +0700

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -2,18 +2,34 @@
 resource "yandex_storage_bucket" "test-bucket" {
   access_key    = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key    = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket        = "test-storage-picture"
+  bucket        = "test-bucket-kibenetiq-yc"
   acl           = "public-read"
   force_destroy = "true"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  # https {
+  #   certificate_id = "yandex_cm_certificate.mydomain.id"
+  # }
+  # website {
+  #   index_document = "index"
+  # }
 }
 
 # Создаем обьект в корзине
 resource "yandex_storage_object" "image-object" {
   access_key    = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key    = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket        = "test-storage-picture"
+  bucket        = yandex_storage_bucket.test-bucket.id
   acl           = "public-read"
   key           = "123.png"
   source        = "data/123.png"
-  depends_on    = [yandex_storage_bucket.test-bucket]
+  depends_on    = [
+    yandex_storage_bucket.test-bucket,
+  ]
 }
